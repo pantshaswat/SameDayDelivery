44f5f5d3dc468309c614a9c94b257d45ae8a2503
Bibek <ba03021321@ku.edu.np>
2024-04-05 22:34:34 +0545

    A

--- SameDayDelivery-Admin/app.js
+++ SameDayDelivery-Admin/app.js
@@ -1,30 +1,31 @@
-//dependencies
 const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+// Add this configuration for CORS
+app.use(cors());
 
-//routes
+// Other middleware and routes
+app.use(express.raw());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.resolve("./public")));
+
+// Your routes
 const orderRoutes = require("./routes/order.routes");
 const userRoutes = require("./routes/user.routes");
 const productRoutes = require("./routes/product.routes");
 
-//middlewares
-app.use(express.raw());
-app.use(bodyParser.urlencoded({ extended: true })); // to support URL
-app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(cookieParser());
-app.use(express.static(path.resolve("./public")));
-
 app.get("/", (req, res) => {
-    res.send("Server Working Correctly...");
+  res.send("Server Working Correctly...");
 });
 
 app.use("/order", orderRoutes);
 app.use("/user", userRoutes);
 app.use("/product", productRoutes);
-
 
 module.exports = app;
